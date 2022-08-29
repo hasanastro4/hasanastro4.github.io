3d57908b5ec2ed16743f2990cdbf356c64465ad9
hasanuddin <hasanuddin@physics.untan.ac.id>
2022-08-29 13:26:49 +0700

    modify header

--- plot.py
+++ plot.py
@@ -3,9 +3,10 @@ import numpy as np
 px = 1/plt.rcParams['figure.dpi']
 plt.subplots(figsize=(890*px,150*px))
 x = np.linspace(0,10,1000)
-y = np.exp(-0.05*x)*np.cos(4*x)
+y = np.exp(-0.1*x)*np.cos(6*x)
 ax = plt.gca()
 plt.plot(x,y)
+plt.plot(x, np.exp(-0.1*x))
 ax.axes.xaxis.set_ticklabels([])
 ax.axes.yaxis.set_ticklabels([])
 ax.set_facecolor('gold')
